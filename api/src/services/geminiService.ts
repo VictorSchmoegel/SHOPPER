bc6a17ef6d1f1b9d7e97e1da0291b8f775d37858
victor schmoegel <victorschmoegel@gmail.com>
2024-08-27 11:26:13 -0300

    criando rota teste

--- api/src/services/geminiService.ts
+++ api/src/services/geminiService.ts
@@ -2,18 +2,15 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const dotenv = require("dotenv");
 dotenv.config();
 
-// Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-async function run() {
-  const prompt = "Write a story about an AI and magic"
-
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
+async function getResponse() {
+  const message = await model.generateContent("Hello, how are you?");
+  const response = await message.response;
   const text = response.text();
   console.log(text);
 }
 
-run();
+getResponse();
